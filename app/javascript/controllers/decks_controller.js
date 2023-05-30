30fb83dd266e54025bc286d0ef7fdf94795773f9
Benjamin Dana <benrdana@gmail.com>
2023-05-30 00:21:51 -0400

    Fixing devise turbo form issues. Updating CSS

--- app/javascript/controllers/decks_controller.js
+++ app/javascript/controllers/decks_controller.js
@@ -11,3 +11,22 @@ export default class extends Controller {
   }
 
 }
+
+$(document).ready(function() {
+  $(document).on("card_result_selected", ".deck-row", function(event) {
+    haveAllCardResultsBeenCompleted();
+  });
+
+  $(document).on("click", ".deck-row", function(event) {
+    var nodeName = event.target.nodeName;
+    console.log("Clicked element's nodeName: " + nodeName);
+    if (nodeName === "BUTTON" || nodeName === "A" || nodeName === "INPUT") {
+      return;
+    } else {
+      // Event triggered only when the background of the row is clicked,
+      // excluding clicks on buttons within the row.
+      $(this).closest('.deck-row').siblings('.deck-row').removeClass('active');
+      $(this).closest('.deck-row').toggleClass('active')
+    }
+  });
+});
