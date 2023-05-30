ee92f190f34a4e9eb44437b06b555535607f4330
Benjamin Dana <benrdana@gmail.com>
2023-05-30 11:54:51 -0400

    Fixing incorrect migration column (duplicates)

--- db/migrate/20230526232317_add_lockable_fields_to_users.rb
+++ db/migrate/20230526232317_add_lockable_fields_to_users.rb
@@ -3,5 +3,7 @@ class AddLockableFieldsToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :failed_attempts, :integer
     add_column :users, :unlocked_token, :string
     add_column :users, :locked_at, :datetime
+
+    add_index :users, :unlock_token, unique: true
   end
 end
--- db/migrate/20230526232522_add_invitable_fields_to_users.rb
+++ db/migrate/20230526232522_add_invitable_fields_to_users.rb
@@ -1,10 +1,5 @@
 class AddInvitableFieldsToUsers < ActiveRecord::Migration[7.0]
   def change
-    # ## Lockable
-    # add_column :users, :failed_attempts, :integer, default: 0, null: false
-    # add_column :users, :unlock_token, :string
-    # add_column :users, :locked_at, :datetime
-
     ## Invitable
     add_column :users, :invitation_token, :string
     add_column :users, :invitation_created_at, :datetime
@@ -14,7 +9,6 @@ class AddInvitableFieldsToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :invited_by_type, :string
     add_column :users, :invited_by_id, :integer
 
-    add_index :users, :unlock_token, unique: true
     add_index :users, :invitation_token, unique: true
   end
 end
